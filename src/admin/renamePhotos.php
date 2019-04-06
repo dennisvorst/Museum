da20bfc7c3b1808fce63114ff7f82cdc9fc88050
Dennis Vorst <dennis.vorst@ziggo.nl>
2019-04-06 08:49:03 +0200

    Renamed the Photos, removed the leading zeros

--- src/admin/renamePhotos.php
+++ src/admin/renamePhotos.php
@@ -5,24 +5,30 @@ require_once("class/File.php");
 $path = "../../db/images/photos";
 $path = "../../db/images/thumbnails";
 
-$photos = scandir($path);
-foreach ($photos as $photo)
-{
-    if (File::endsWith($photo, ".jpg")) 
-    {
+$paths = ["../../db/images/photos", "../../db/images/thumbnails"];
 
-        if (file_exists($path . "/" . $photo) )
-        {
-            /** rename it */
-            $newPhotoName = createNewFileName($photo);
-            echo "renaming " . $photo . " into " . $newPhotoName .  "</br>";
-            rename ($path . "/" . $photo, $path . "/" . $newPhotoName);
+foreach ($paths as $path)
+{
+	$photos = scandir($path);
+	foreach ($photos as $photo)
+	{
+		if (File::endsWith($photo, ".jpg")) 
+		{
+
+			if (file_exists($path . "/" . $photo) )
+			{
+				/** rename it */
+				$newPhotoName = createNewFileName($photo);
+				echo "renaming " . $photo . " into " . $newPhotoName .  "</br>";
+				rename ($path . "/" . $photo, $path . "/" . $newPhotoName);
     
-        } else {
-            echo $photo . " niet gevonden</br>";
-        }
-    }
+			} else {
+				echo $photo . " niet gevonden</br>";
+			}
+		}
+	}
 }
+
 
 function createNewFileName($photo){
     /** find the value and that is the start of the file name  
