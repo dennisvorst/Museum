562f2b388039224564e3ab45786c57936143cc56
Dennis Vorst <dennis.vorst@ziggo.nl>
2021-02-16 08:20:49 +0100

    add country to the sources table and add a fk contraint with the countries table #13

--- README.md
+++ README.md
@@ -1,18 +1,23 @@
 # Museum
 Website for the Dutch base- and softballmuseum.
 
+this webstite is hosted on www.honkbalmuseum.nl
+
+
 ## 3rd Party sofware used on the website 
 
 | Name | Source |
-|-|-|-|
+|-|-|
 | Font Awesome | https://fontawesome.com/ |
 | Bootstrap | https://getbootstrap.com/ |
 
 ## 3rd Party development tools used
 
 | Name | Source |
-|-|-|-|
+|-|-|
 | XXAMP | https://www.apachefriends.org/index.html |
 | Composer | https://getcomposer.org/ |
 | phpunit | https://phpunit.de/ |
 | PHINX | | https://phinx.org/ |
+
+
---
+++ db/build_scripts/20210216_10_update sources add fk to countries.sql
@@ -0,0 +1,12 @@
+start transaction;
+ALTER TABLE sources ADD cdcountry CHAR(5) NOT NULL DEFAULT 'NLD' AFTER nmcity; 
+
+ALTER TABLE sources
+  ADD CONSTRAINT country_sources_1 FOREIGN KEY (cdcountry) REFERENCES countries (cdcountry);
+commit
+
+
+UPDATE `sources` SET `cdcountry` = 'USA' WHERE `sources`.`idsource` = 33; 
+UPDATE `sources` SET `cdcountry` = 'USA' WHERE `sources`.`idsource` = 39; 
+UPDATE `sources` SET `cdcountry` = 'DEU' WHERE `sources`.`idsource` = 42; 
+UPDATE `sources` SET `cdcountry` = 'CUW' WHERE `sources`.`idsource` = 52; 
