3f66c0d0c116ee2d82c9b8d1d0c5a2ff64c31f74
Dennis Vorst <dennis.vorst@ziggo.nl>
2021-06-05 20:42:37 +0200

    Admin update

--- src/admin/index.php
+++ src/admin/index.php
@@ -1,49 +1,109 @@
 <?php
-//require_once('class/Log.php');
-//require_once('class/MysqlConfig.php');
-//require_once('class/MysqlDatabase.php');
-require_once "class/Mysql.php";
+require_once('class/File.php');
+require_once('class/CheckPhotos.php');
+require_once('class/CheckSources.php');
+require_once('class/CheckCompetitions.php');
 
-$log = new Log("museum.log");
-$config = new MysqlConfig();
-$db = new MysqlDatabase($config, "museum", $log);
-
-$keys = array_keys($_GET);
-foreach($keys as $key)
+// https://blog.bobbyallen.me/2013/03/23/using-composer-in-your-own-php-projects-with-your-own-git-packageslibraries/
+if ($_SERVER['SERVER_NAME'] == "localhost")
 {
-    ${$key} = $_GET[$key];
+    require '../../vendor/autoload.php';
+} else {
+    // production servers
+    require_once 'autoload/MysqlDatabase/Log.php';
+    require_once 'autoload/MysqlDatabase/MysqlConfig.php';
+    require_once 'autoload/MysqlDatabase/MysqlDatabase.php';
 }
-?>
-<html>
-    <head>
-    </head>
-    <body>
-        <?php
 
-        if (isset($server)) {
-            if (isset($table)) {
-                /** Show the table content
-                 *  sorting can only be done on tables
-                 */
-                $table = ['TABLE_SCHEMA' => $server, 'TABLE_NAME' => $table];
-                if (isset($orderby))
-                {
-                    $mysql = new MysqlTable($db, $table, $orderby, $direction);
-                } else {
-                    $mysql = new MysqlTable($db, $table);
-                }
-                echo $mysql->showRecordsPage();
-            } else {
-                /** show the database content  */
-                $server = ['SCHEMA_NAME' => $server];
-                $mysql = new MysqlServer($db, $server);
-                echo $mysql->showTablesPage();
-            }
-        } else {
-            /** show the server content */
-            $mysql = new Mysql($db);
-            echo $mysql->showServersPage();
+/** init  */
+$option = 0;
+if (isset($_GET['option']))
+{
+    $option = $_GET['option'];
+}
+
+/** databaseconnection  */
+$log = new Log("museum.log");
+
+switch ($_SERVER['SERVER_NAME'])
+{
+    case "localhost":
+        /* localhost */
+        $mysqli = new Mysqli("localhost", "root", "", "museum");
+        break;
+    case "www.honkbalmuseum.nl":
+        include "inc/www.honkbalmuseum.nl.inc";
+        break;
+    case "test.honkbalmuseum.nl":
+        include "inc/test.honkbalmuseum.nl.inc";
+        break;
+}
+
+try {
+    $db = new MysqlDatabase($mysqli, $log);
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+    return;
+}
+
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <?php
+        switch ($option)
+        {
+
+            case "0":
+                echo showMenu();
+                break;
+            case "1":
+                //checkPhotos
+                $obj = new CheckPhotos($db);
+                echo $obj->show(); 
+                break;
+            case "2":
+                //checkSources
+                $obj = new CheckSources($db);
+                echo $obj->show(); 
+                break;
+            case "3":
+                //generateSeedContent
+                return $this->generateSeedContent();
+                break;
+            case "4":
+                //checkCompetitions
+                $obj = new CheckCompetitions($db);
+                echo $obj->show(); 
+                break;
         }
-        ?>
+    ?>
+
     </body>
 </html>
+
+<?php
+    function showMenu() : string 
+    {
+        return "
+        <h1>Menu</h1>
+
+        <ul>
+            <li><a href='index.php?option=1'>Check photos</a></li>
+            <li><a href='index.php?option=2'>Check sources</a></li>
+            <li><a href='index.php?option=3'>Generate seed content</a></li>
+
+            <li><a href='index.php?option=4'>Check Competitions</a></li>
+        </ul>\n";
+    }
+?>
