7baf61c964e4ee118d00d1202080365634580a2f
Dennis Vorst <dennis.vorst@ziggo.nl>
2021-07-13 20:03:13 +0200

    MVC Test Implementation

--- tests/Unit/VideoViewTest.php
+++ tests/Unit/VideoViewTest.php
@@ -9,5 +9,24 @@ class VideoViewTest extends TestCase
     {
         $this->assertTrue(class_exists("VideoView"));
     }
+
+    public function testClassVideoViewCanBeInstatiated()
+    {
+      $video['id'] = 1;
+      $video['name'] = "Angels in the outfield";
+      $video['url'] = "qwerty";
+
+      $video['video'] = $video;
+
+  		$object = new VideoView($video);
+	  	$this->assertInstanceOf(VideoView::class, $object);	
+    }
+
+    public function testEmptyVideoThrowsException()
+    {
+      $this->expectException(InvalidArgumentException::class);
+
+      $object = new VideoView([]);
+    }
 }
 ?>
