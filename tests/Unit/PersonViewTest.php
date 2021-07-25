c58db0c7ae76abf175a144181446a241534f6d55
Dennis Vorst <dennis.vorst@ziggo.nl>
2021-07-25 15:29:34 +0200

    MVC test implementation

--- tests/Unit/PersonViewTest.php
+++ tests/Unit/PersonViewTest.php
@@ -48,6 +48,95 @@ class PersonViewTest extends TestCase
 		$object = new PersonView($this->_row);
 		$actual = $object->showThumbnail();
 		$this->assertIsString($actual);	
+
+	}
+
+	/** _getMugshotPhoto */
+	public function testFunctionGetMugshotPhotoWithoutAnyPhotosThrowsException()
+	{
+		/** empty halloffame photo and empty collections */
+		$this->expectException(Exception::class);
+		$object = new PersonView($this->_row);
+		$actual = $object->getMugshotPhoto();
+	}
+
+
+	public function testFunctionGetMugshotPhotoWithoutAnyMugshotThrowsException()
+	{
+		$this->expectException(Exception::class);
+	
+		/** empty halloffame photo and collections with a photo that is not a mugshot */
+		$photo['photo']['id'] = 2;
+		$photo['photo']['isMugshot'] = false;
+
+		$row['person']['id'] = 1;
+		$row['person']['firstName'] = "Hank";
+		$row['person']['lastName'] = "Aaron";
+		$row['photos'][] = $photo;
+
+		$object = new PersonView($row);
+		$actual = $object->getMugshotPhoto();
+	}
+
+
+	public function testFunctionGetMugshotPhoto()
+	{
+		/** returns hallof fame photo when halloffame photo is provided  */
+		/** Arrange */ 
+		$row['person']['id'] = 1;
+		$row['person']['firstName'] = "Hank";
+		$row['person']['lastName'] = "Aaron";
+		$row['person']['lastName'] = "Aaron";
+		$row['hallOfFame']['photo']['id'] = 1;
+		$row['hallOfFame']['date'] = "20202002";
+
+		/** Act */
+		$object = new PersonView($row);
+		$actual = $object->getMugshotPhoto();
+
+		/** assert of type PhotoView */
+		$this->assertInstanceOf(PhotoView::class, $actual);
+
+		/** Assert */
+		$actual = $actual->showPhoto();
+		$expected = "<img src='./images/photos/1.jpg' class='rounded'>";
+		$this->assertEquals($actual, $expected);
+
+		/** returns first photo when no hall of fame photo is provided and the first itemin the collection is of type mugshot */
+		/** Arrange */ 
+		unset($row['hallOfFame']);
+		$photo['photo']['id'] = 2;
+		$photo['photo']['isMugshot'] = true;
+		$row['photos'][] = $photo;
+
+		/** Act */
+		$object = new PersonView($row);
+		$actual = $object->getMugshotPhoto();
+
+		/** assert of type PhotoView */
+		$actual = $actual->showPhoto();
+		$expected = "<img src='./images/photos/2.jpg' class='rounded'>";
+		$this->assertEquals($actual, $expected);
+
+		// /** returns second photo when no hall of fame photo is provided and the first item in the collection is not of type mugshot but the second is */
+		// /** Arrange */ 
+		// unset($row['photos']);
+		// $photo['photo']['id'] = 2;
+		// $photo['photo']['isMugshot'] = false;
+		// $row['photos'][] = $photo;
+		// $photo['photo']['id'] = 3;
+		// $photo['photo']['isMugshot'] = true;
+		// $row['photos'][] = $photo;
+
+		// /** Act */
+		// $object = new PersonView($row);
+		// $actual = $object->getMugshotPhoto();
+
+		// /** assert of type PhotoView */
+		// $actual = $actual->showPhoto();
+		// $expected = "<img src='./images/photos/3.jpg' class='rounded'>";
+		// $this->assertEquals($actual, $expected);
+		
 	}
 }
 ?>
